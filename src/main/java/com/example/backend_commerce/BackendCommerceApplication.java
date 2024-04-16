2b540a9dd40137a623390e7eb9672c026c427b56
MinhHieuVux <minhhieuvux@gmail.com>
2024-04-16 21:23:34 +0700

    first commit

---
+++ src/main/java/com/example/backend_commerce/BackendCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendCommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backend_commerce/BackendCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
